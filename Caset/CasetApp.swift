b98adbfe6027d6c14bf831f7d23b58bb9cf16a97
judealatawi <judealatawi@gmail.com>
2023-02-07 15:06:15 +0300

    Initial Commit

---
+++ Caset/CasetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasetApp.swift
+//  Caset
+//
+//  Created by Jude Alatawi on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CasetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
